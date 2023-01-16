041cba4259e26d778d9e219cb6d1198df9b031e1
Nikita Doroshenko <nikitokd05@gmail.com>
2023-01-16 17:20:54 +0200

    added sections advantages, explore, slider and order

--- gulpfile.js
+++ gulpfile.js
@@ -31,6 +31,8 @@ function styles(params) {
 function scripts(params) {
   return src([
     'node_modules/jquery/dist/jquery.js',
+    'node_modules/slick-carousel/slick/slick.js',
+    'node_modules/rateyo/src/jquery.rateyo.js',
     'app/js/main.js'
   ])
   .pipe(concat('main.min.js'))
